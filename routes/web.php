87fc720ff52eb7c9fa3ea6771f8f670156298dfb
l3lackheart <dgbao1340@gmail.com>
2020-03-05 19:17:36 +0700

    feat: add html files

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,18 @@ Route::group([
     'namespace' => 'Admin'
 ], function () {
 
-    Route::group(['prefix' => 'products'], function() {
+    Route::resource('categories', 'CategoryController');
+    Route::get('', 'DashboardController');
+    Route::get('login', 'LoginController@showLoginForm');
+    Route::post('login', 'LoginController@login');
+    Route::post('logout', 'LoginController@logout');
+    Route::group(['prefix' => 'orders'], function () {
+        Route::get('', 'OrderController@index');
+        Route::get('processed', 'OrderController@processed');
+        Route::get('{order}/edit', 'OrderController@edit');
+        Route::put('{order}', 'OrderController@update');
+    });
+    Route::group(['prefix' => 'products'], function () {
         Route::get('', 'ProductController@index');
         Route::get('create', 'ProductController@create');
         Route::post('', 'ProductController@store');
@@ -29,8 +40,5 @@ Route::group([
         Route::delete('{product}', 'ProductController@destroy');
         Route::get('{product}', 'ProductController@show');
     });
-
     Route::resource('users', 'UserController');
-
 });
-
